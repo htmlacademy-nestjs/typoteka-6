6513a26497b4c85f8072d251876fa64249985dcd
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-17 16:18:49 +0300

    1.4. Добавит основу для сервиса `file-vault`
    Подготовим заготовку для следующего микросервиса `file-vault`. Он будет отвечать за хранение пользовательских файлов. Для этого воспользуемся генератором проектов Nest:
    
    ```
    npx nx g @nx/nest:application file-vault
    ```
    
    После создания нового сервиса, удалим заготовку для e2e-тестов.

---
+++ project/apps/file-vault/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'file-vault',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/file-vault',
+};
