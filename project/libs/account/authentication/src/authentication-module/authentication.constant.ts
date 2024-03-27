b7f1d9f5f82267450fd6987d05588dfd4d6ce551
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 19:12:32 +0300

    2.21. Добавит метод регистрации в `AuthenticationService`.
    Для регистрации пользователей определим новый метод в сервисе
    `AuthenticationService` и напишем в нём всё необходимую логику.
    
    Метод `register` принимает аргументом `dto` типа `CreateUserDto`.
    Внутри метода извлекаем все необходимые поля, а затем начинаем
    взаимодействовать с репозиторием.
    
    Перед добавлением информации о новом пользователе, выполним проверку.
    В базе не может быть двух пользователей с одинаковым email.

---
+++ project/libs/account/authentication/src/authentication-module/authentication.constant.ts
@@ -0,0 +1,1 @@
+export const AUTH_USER_EXISTS = 'User with this email exists';
--- project/libs/account/blog-user/src/blog-user-module/blog-user.repository.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.repository.ts
@@ -10,4 +10,15 @@ export class BlogUserRepository extends BaseMemoryRepository<BlogUserEntity> {
   constructor(entityFactory: BlogUserFactory) {
     super(entityFactory);
   }
+
+  public async findByEmail(email: string): Promise<BlogUserEntity | null> {
+    const entities = Array.from(this.entities.values());
+    const user = entities.find((entity) => entity.email === email);
+
+    if (! user) {
+      return null;
+    }
+
+    return this.entityFactory.create(user);
+  }
 }
