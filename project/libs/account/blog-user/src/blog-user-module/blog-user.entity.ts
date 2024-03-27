05c08eeac304093c6febcc97f4733c9dafc857ec
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 18:35:25 +0300

    2.11. Добавит имплементацию `BlogUserEntity` и `BlogUserFactory`.
    Имплементируем подготовленные интерфейсы.

---
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
@@ -0,0 +1,42 @@
+import { Entity } from '@project/shared/core';
+import { StorableEntity, AuthUser, UserRole} from '@project/shared/core';
+
+export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
+  public email: string;
+  public firstname: string;
+  public lastname: string;
+  public dateOfBirth: Date;
+  public role: UserRole;
+  public passwordHash: string;
+
+  constructor(user?: AuthUser) {
+    super();
+    this.populate(user);
+  }
+
+  public populate(user?: AuthUser): void {
+    if (! user) {
+      return;
+    }
+
+    this.id = user.id ?? '';
+    this.email = user.email;
+    this.dateOfBirth = user.dateOfBirth;
+    this.firstname = user.firstname;
+    this.lastname = user.lastname;
+    this.passwordHash = user.passwordHash;
+    this.role = user.role;
+  }
+
+  public toPOJO(): AuthUser {
+    return {
+      id: this.id,
+      email: this.email,
+      firstname: this.firstname,
+      lastname: this.lastname,
+      dateOfBirth: this.dateOfBirth,
+      role: this.role,
+      passwordHash: this.passwordHash,
+    }
+  }
+}
