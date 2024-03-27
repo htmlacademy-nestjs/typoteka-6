0dbb27c86a632243f1bca72b1f268c4fd20ffedb
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 19:24:26 +0300

    2.26. Добавит обработчики маршрутов `/login`, `/:id`.
    Аналогичным образом добавим обработчики для маршрутов `/login`,
    `/:id`. Внутри обработчиков происходит обращение к соответствующим
    методом сервиса `AuthenticationService`.

--- project/libs/account/authentication/src/authentication-module/authentication.controller.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from '../dto/create-user.dto';
+import { LoginUserDto } from '../dto/login-user.dto';
 
 @Controller('auth')
 export class AuthenticationController {
@@ -13,5 +14,17 @@ export class AuthenticationController {
   public async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
     return newUser.toPOJO();
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoginUserDto) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return verifiedUser.toPOJO();
+  }
+
+  @Get(':id')
+  public async show(@Param('id') id: string) {
+    const existUser = await this.authService.getUser(id);
+    return existUser.toPOJO();
   }
 }
