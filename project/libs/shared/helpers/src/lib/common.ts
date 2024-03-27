9cd4d83ab63bf2fb2215bfa4dc524ccc7a90b283
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 10:20:19 +0300

    2.6. Добавит модуль common.ts в общую библиотеку shared/helpers.
    Потренируемся работать с общей библиотекой `shared/helpers`. Создадим новый
    модуль `common.ts` и опишем в нём первую функцию — `fillDto`. Эта функция упростит использование `plainToInstance` из пакета `class-transformer`. Аргументами она принимает любую DTO/RDO
    (в виде класса) и объект с данными.
    
    После создания модуля `common.ts`, не забудем обновить `index.ts` и добавить реэкспорт.

---
+++ project/libs/shared/helpers/src/lib/common.ts
@@ -0,0 +1,26 @@
+import { ClassTransformOptions, plainToInstance } from 'class-transformer';
+
+type PlainObject = Record<string, unknown>;
+
+export function fillDto<T, V extends PlainObject>(
+  DtoClass: new () => T,
+  plainObject: V,
+  options?: ClassTransformOptions,
+): T;
+
+export function fillDto<T, V extends PlainObject[]>(
+  DtoClass: new () => T,
+  plainObject: V,
+  options?: ClassTransformOptions,
+): T[];
+
+export function fillDto<T, V extends PlainObject>(
+  DtoClass: new () => T,
+  plainObject: V,
+  options?: ClassTransformOptions,
+): T | T[] {
+  return plainToInstance(DtoClass, plainObject, {
+      excludeExtraneousValues: true,
+      ...options,
+  });
+}
