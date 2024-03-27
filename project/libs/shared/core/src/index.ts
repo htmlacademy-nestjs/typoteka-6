eab3327b40385f7d64943aea99871a82c94347e7
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 18:33:06 +0300

    2.10. Добавит интерфейс `StorableEntity`.
    Интерфейс предусматривает метод `toPOJO`, который будет возвращать простой объект для Entity.
    
    Интерфейс `EntityFactory` пригодится для описания фабрик — классов, которые отвечают за создание экземпляров классов.

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -3,3 +3,7 @@ export { Entity } from './lib/base/entity';
 export { UserRole } from './lib/types/user-role.enum';
 export { User } from './lib/types/user.interface';
 export { AuthUser } from './lib/types/auth-user.interface';
+
+export { StorableEntity } from './lib/interfaces/storable-entity.interface';
+export { EntityFactory } from './lib/interfaces/entity-factory.interface';
+
