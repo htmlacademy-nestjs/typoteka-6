7a706f203f35e9f5e1597c915286fa867b9b49e2
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 19:12:32 +0300

    2.21. Добавит метод регистрации в `AuthenticationService`.
    Для регистрации пользователей определим новый метод в сервисе
    `AuthenticationService` и напишем в нём всё необходимую логику.
    
    Метод `register` принимает аргументом `dto` типа `CreateUserDto`.
    Внутри метода извлекаем все необходимые поля, а затем начинаем
    взаимодействовать с репозиторием.
    
    Перед добавлением информации о новом пользователе, выполним проверку.
    В базе не может быть двух пользователей с одинаковым email.

--- project/libs/account/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.service.ts
@@ -1,10 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import dayjs from 'dayjs';
+import { ConflictException, Injectable } from '@nestjs/common';
 
-import { BlogUserRepository } from '@project/blog-user';
+import { BlogUserRepository, BlogUserEntity } from '@project/blog-user';
+import { UserRole } from '@project/shared/core';
+
+import { CreateUserDto } from '../dto/create-user.dto';
+import { AUTH_USER_EXISTS } from './authentication.constant';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     private readonly blogUserRepository: BlogUserRepository
   ) {}
+
+  public async register(dto: CreateUserDto) {
+    const {email, firstname, lastname, password, dateBirth} = dto;
+
+    const blogUser = {
+      email, firstname, lastname, role: UserRole.User,
+      avatar: '', dateOfBirth: dayjs(dateBirth).toDate(),
+      passwordHash: ''
+    };
+
+    const existUser = await this.blogUserRepository
+      .findByEmail(email);
+
+    if (existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser)
+      .setPassword(password)
+
+    return this.blogUserRepository
+      .save(userEntity);
+  }
 }
--- project/libs/account/blog-user/src/blog-user-module/blog-user.repository.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.repository.ts
@@ -10,4 +10,10 @@ export class BlogUserRepository extends BaseMemoryRepository<BlogUserEntity> {
   constructor(entityFactory: BlogUserFactory) {
     super(entityFactory);
   }
+
+  public findByEmail(email: string): Promise<BlogUserEntity | null> {
+    const entities = Array.from(this.entities.values());
+    const user = entities.find((entity) => entity.email === email);
+    return Promise.resolve(this.entityFactory.create(user));
+  }
 }
