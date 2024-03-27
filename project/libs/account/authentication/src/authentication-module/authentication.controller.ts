647acfc4ae2c417f2378ba643adb27eef6266fcc
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 19:22:27 +0300

    2.25. Добавит обработчик маршрута `/register`.
    Добавим обработчик для маршрута `/auth/register`. В нём мы реализуем
    сценарий регистрации (создания) нового пользователя.
    
    Этот обработчик ожидает запрос, отправленный методом POST. Чтобы
    закрепить за обработчиком нужный метод, мы используем декоратор
    `@Post`. Аргументом передаём маршрут (`/register`).
    
    Данные для регистрации нового пользователя клиент передаёт в теле
    запроса. Для их извлечения применяется другой декоратор — `@Body`. Им
    необходимо воспользоваться при определении параметра для обработчика.
    В нашем примере, данные из тела запроса будут помещены в параметр `dto`.
    
    Затем происходит обращение к сервису `AuthenticationService`. Вызывается
    одноимённый метод для регистрации пользователя, а затем возвращается
    результат в виде заполненного response object. Для заполнения
    используем функцию `fillDto` из общей библиотеки `@project/shared/helpers`

--- project/libs/account/authentication/src/authentication-module/authentication.controller.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.controller.ts
@@ -1,10 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 import { AuthenticationService } from './authentication.service';
+import { CreateUserDto } from '../dto/create-user.dto';
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(
     private readonly authService: AuthenticationService
   ) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authService.register(dto);
+    return newUser.toPOJO();
+  }
 }
