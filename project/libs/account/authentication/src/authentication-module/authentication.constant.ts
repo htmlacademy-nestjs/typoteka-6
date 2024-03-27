cbe8ac9760a1a8b24d7a505a7fe409b9a80b923b
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 19:13:52 +0300

    2.22. Добавит метод `verifyUser` в `AuthenticationService`.
    Аналогичным образом добавим в сервис `AuthService` методы `verifyUser`
    и `getUser`. В первом выполним проверку логина и пароля
    пользователя. Для этого соберём сущность пользователя и воспользуемся
    методом `comparePassword`.
    
    В методе `getUser` реализуем получение пользователя по уникальному
    идентификатору.

--- project/libs/account/authentication/src/authentication-module/authentication.constant.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.constant.ts
@@ -1,1 +1,3 @@
 export const AUTH_USER_EXISTS = 'User with this email exists';
+export const AUTH_USER_NOT_FOUND = 'User not found';
+export const AUTH_USER_PASSWORD_WRONG = 'User password is wrong';
