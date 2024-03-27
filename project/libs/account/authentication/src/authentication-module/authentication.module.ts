902fff58a9b2b4405308ba15b3b9d918ee71ca3f
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 18:54:19 +0300

    2.15. Внедрит репозиторий в качестве зависимости.
    Теперь посмотрим как работает внедрение зависимостей в Nest.
    Репозиторий `BlogUserRepository` потребуется внутри сервиса
    `AuthenticationService`.
    
    Для внедрения его в качестве зависимости, достаточно определить в
    конструкторе параметр соответствующего типа. Остальное Nest сделать
    автоматически.

--- project/libs/account/authentication/src/authentication-module/authentication.module.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { BlogUserModule } from '@project/blog-user';
+
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
+  imports: [BlogUserModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService]
 })
