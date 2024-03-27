b4ecf0b57b5fc130c6fa9c5cbbf3474c9f3b5f51
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 10:01:14 +0300

    2.3. Добавит интерфейс `User`, `AuthUser` и перечисление `UserRole`, `Entity`.
    В этом разделе мы частично разработаем сервис `account`. Нам потребуется интерфейс для определения сущности «Пользователь». Создадим его в общей библиотеке `core`.
    
    Файл с интерфейсом (`user.interface.ts`) разместим в директории
    `core/types/src/lib`. Здесь же создадим первое перечисление
    `UserRole`. В этом перечислении отразим возможные роли пользователей.
    
    Техническое задание определяет две роли: «Администраторы»
    и «Пользователи».
    
    Обратите внимание на именование интерфейса `User`. В имени интерфейса не используется постфикс `Interface` или буквы `I`. Это ещё один из вариантов задания имени для интерфейсов. Для имени интерфейса используются короткие имена, а для классов, которые реализуют его, более ёмкие.
    
    Например, интерфейс для определения пользователя называется «User», а класс, который его реализует, может называться `BlogUser`. В имени класса учитывается предметная область — блог.
    
    В своих проектах вы можете выбрать любой из вариантов именования,
    главное чтобы он использовался во всём проекте.
    
    Также добавим отдельный интерфейс `AuthUser`. Он наследуется от `User` и содержит дополнительное поле `passwordHash`. В нём будем хранить хешированную версию параля.
    
    Последним шагом, отредактируем файл `index.ts` и сделаем реэкспорт
    созданных элементов.
    
    Чтобы воспользоваться общими интерфейсами, мы должны импортировать их из общей библиотеки. Вместо полного пути к библиотеке указывается псевдоним (алиас).
    
    Псевдонимы для путей формируются при создании общих библиотек.
    Посмотреть список созданных псевдонимов вы можете в конфигурационном файле `tsconfig.base.json`, секция `paths`.
    
    Для первой библиотеки создан псевдоним `@project/shared/core`. Если
    в каком-то из сервисов потребуется импортировать интерфейс (например, `User`), то это нужно делать, опираясь на псевдоним:
    
    ```
    import {User} from '@project/shared/core';

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -1,1 +1,5 @@
-// Ре-экспорт модулей из `./lib`
+export { Entity } from './lib/base/entity';
+
+export { UserRole } from './lib/types/user-role.enum';
+export { User } from './lib/types/user.interface';
+export { AuthUser } from './lib/types/auth-user.interface';
