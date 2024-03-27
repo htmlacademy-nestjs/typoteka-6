cf4b97059624862566aeab0683f54b1637b518ae
Igor Antonov <antonov.igor.khv@gmail.com>
2024-03-27 19:13:52 +0300

    2.22. Добавит метод `verifyUser` в `AuthenticationService`.
    Аналогичным образом добавим в сервис `AuthService` методы `verifyUser`
    и `getUser`. В первом выполним проверку логина и пароля
    пользователя. Для этого соберём сущность пользователя и воспользуемся
    методом `comparePassword`.
    
    В методе `getUser` реализуем получение пользователя по уникальному
    идентификатору.

--- project/libs/account/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.service.ts
@@ -1,11 +1,12 @@
 import dayjs from 'dayjs';
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 
 import { BlogUserRepository, BlogUserEntity } from '@project/blog-user';
 import { UserRole } from '@project/shared/core';
 
 import { CreateUserDto } from '../dto/create-user.dto';
-import { AUTH_USER_EXISTS } from './authentication.constant';
+import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './authentication.constant';
+import { LoginUserDto } from '../dto/login-user.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -34,5 +35,30 @@ export class AuthenticationService {
 
     return this.blogUserRepository
       .save(userEntity);
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const {email, password} = dto;
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    if (!await existUser.comparePassword(password)) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return existUser;
+  }
+
+  public async getUser(id: string) {
+    const user = await this.blogUserRepository.findById(id);
+
+    if (! user) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    return user;
   }
 }
