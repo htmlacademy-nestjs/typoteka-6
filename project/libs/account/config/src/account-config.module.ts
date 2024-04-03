1b583e4dc5b8642d20e9191bf0e8381f3f01a04a
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-03 17:42:38 +0300

    3.7. Добавит пространство `application` с настройками приложения. WIP
    Чем больше функциональности предоставляет приложение, тем больше входных данных ему может потребоваться. Модуль для управления конфигурацией позволяет загружать несколько пользовательских файлов конфигурации. Чтобы проще взаимодействовать с загруженными настройками,
    их можно разделить по пространствам имён. Проще говоря: сгруппировать.
    
    Для этого необходимо воспользоваться фабрикой `registerAs`. Технически это обычная функция. Она возвращает объект с настройками для определённого пространства. Рассмотрим
    пример создания пространства имён `application`. Опишем функцию `registerAs` в отдельном модуле `app.config.ts`.
    
    Аргументом функции передадим название нового пространства имён — `application`. Именно через это имя мы будем обращаться для получения общих настроек приложения.
    
    Вторым аргументом функция принимает колбэк. Он должен вернуть объект с настройками. Для получения настроек обратимся к объекту `process.env`.
    
    Здесь же мы можем предусмотреть настройки по умолчанию, выполнить преобразование типов (не забывайте, настройки изначально представлены в виде строк).
    
    WIP. Приложение по-прежнему не запускается, не подготовлена вторая конфигурация.

--- project/libs/account/config/src/account-config.module.ts
+++ project/libs/account/config/src/account-config.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import applicationConfig from './configurations/app.config';
+
 const ENV_USERS_FILE_PATH = 'apps/account/account.env';
 
 @Module({
@@ -8,8 +10,7 @@ const ENV_USERS_FILE_PATH = 'apps/account/account.env';
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      // TODO: Передать список конфигураций для загрузки
-      load: [],
+      load: [applicationConfig],
       envFilePath: ENV_USERS_FILE_PATH
     }),
   ]
---
+++ project/libs/account/config/src/configurations/app.config.ts
@@ -0,0 +1,36 @@
+import { registerAs } from '@nestjs/config';
+import * as Joi from 'joi';
+
+const DEFAULT_PORT = 3000;
+const ENVIRONMENTS = ['development', 'production', 'stage'] as const;
+
+type Environment = typeof ENVIRONMENTS[number];
+
+export interface ApplicationConfig {
+  environment: string;
+  port: number;
+}
+
+const validationSchema = Joi.object({
+  environment: Joi.string().valid(...ENVIRONMENTS).required(),
+  port: Joi.number().port().default(DEFAULT_PORT),
+});
+
+function validateConfig(config: ApplicationConfig): void {
+  const { error } = validationSchema.validate(config, { abortEarly: true });
+  if (error) {
+    throw new Error(`[Application Config Validation Error]: ${error.message}`);
+  }
+}
+
+function getConfig(): ApplicationConfig {
+  const config: ApplicationConfig = {
+    environment: process.env.NODE_ENV as Environment,
+    port: parseInt(process.env.PORT || `${DEFAULT_PORT}`, 10),
+  };
+
+  validateConfig(config);
+  return config;
+}
+
+export default registerAs('application', getConfig);
