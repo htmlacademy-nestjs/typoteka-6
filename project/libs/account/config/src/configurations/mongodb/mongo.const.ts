2f335f274c0113b7297116dcc80303b1562ec453
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-03 18:06:06 +0300

    3.11. Добавит валидацию с помощью `class-validator`.
    У любой задачи несколько решений. Данная — не исключения. `ConfigModule` поддерживают работу с `joi` из коробки, но ничего не мешает воспользоваться уже знакомым и привычным `class-validator`. Рассмотрим альтернативное решение.
    
    Для начала установим `class-validator` в основные зависимости.
    
    Создадим в библиотеке файл `mongo.env.ts` и опишем в нём класс `MongoConfiguration`. Опишем в этом классе поля, имена которых соответствуют названиям переменных окружения.
    
    Затем воспользуемся декораторами для валидации из пакета
    `class-validator`. Обратите внимание на комбинацию декораторов для
    проверки `MONGO_PORT`. `class-validator` предоставляет готовый
    декоратор `IsPort()`, но он работает со строками. Поэтому мы добиваемся аналогичного эффекта путём комбинации нескольких декораторов.

---
+++ project/libs/account/config/src/configurations/mongodb/mongo.const.ts
@@ -0,0 +1,3 @@
+export const MIN_PORT = 0;
+export const MAX_PORT = 65535;
+export const DEFAULT_MONGO_PORT = 27017;
---
+++ project/libs/account/config/src/configurations/mongodb/mongo.messages.ts
@@ -0,0 +1,8 @@
+export enum EnvValidationMessage {
+  DBHostRequired = 'MongoDB host is required',
+  DBNameRequired = 'Database name is required',
+  DBPortRequired = 'MongoDB port is required',
+  DBUserRequired = 'MongoDB user is required',
+  DBPasswordRequired = 'MongoDB password is required',
+  DBBaseAuthRequired = 'MongoDB authentication base is required',
+}
