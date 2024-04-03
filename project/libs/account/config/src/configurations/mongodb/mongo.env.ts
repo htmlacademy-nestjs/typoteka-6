063686b92078fa21ff94eab9e488bc5d5a812468
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-03 18:06:06 +0300

    3.11. Добавит валидацию с помощью `class-validator`.
    У любой задачи несколько решений. Данная — не исключения. `ConfigModule` поддерживают работу с `joi` из коробки, но ничего не мешает воспользоваться уже знакомым и привычным `class-validator`. Рассмотрим альтернативное решение.
    
    Для начала установим `class-validator` в основные зависимости.
    
    Создадим в библиотеке файл `mongo.env.ts` и опишем в нём класс `MongoConfiguration`. Опишем в этом классе поля, имена которых соответствуют названиям переменных окружения.
    
    Затем воспользуемся декораторами для валидации из пакета
    `class-validator`. Обратите внимание на комбинацию декораторов для
    проверки `MONGO_PORT`. `class-validator` предоставляет готовый
    декоратор `IsPort()`, но он работает со строками. Поэтому мы добиваемся аналогичного эффекта путём комбинации нескольких декораторов.

---
+++ project/libs/account/config/src/configurations/mongodb/mongo.env.ts
@@ -0,0 +1,31 @@
+import { IsNumber, IsOptional, IsString, Max, Min, validateOrReject } from 'class-validator';
+
+import { EnvValidationMessage } from './mongo.messages';
+import { MIN_PORT, MAX_PORT, DEFAULT_MONGO_PORT } from './mongo.const';
+
+export class MongoConfiguration {
+  @IsString({ message: EnvValidationMessage.DBNameRequired })
+  public name: string;
+
+  @IsString({ message: EnvValidationMessage.DBHostRequired })
+  public host: string;
+
+  @IsNumber({}, { message: EnvValidationMessage.DBPortRequired })
+  @Min(MIN_PORT)
+  @Max(MAX_PORT)
+  @IsOptional()
+  public port: number = DEFAULT_MONGO_PORT;
+
+  @IsString({ message: EnvValidationMessage.DBUserRequired })
+  public user: string;
+
+  @IsString({ message: EnvValidationMessage.DBPasswordRequired })
+  public password: string;
+
+  @IsString({ message: EnvValidationMessage.DBBaseAuthRequired })
+  public authBase: string;
+
+  public async validate(): Promise<void> {
+    await validateOrReject(this);
+  }
+}
