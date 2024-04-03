741ccf84cb3825eba4a1a9c5ffef66252d141fdb
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-03 19:15:18 +0300

    3.22. Внедрим модель в `BlogUserModule`.
    Вернёмся в модуль `BlogUserModule` и добавим внедрение модели. Её мы будем использовать при имплементации репозитория для `BlogUser`.
    
    Чтобы внедрить модель, воспользуемся `MongooseModule`. В метод `forFeature` передадим единственную нашу модель.

--- project/libs/account/blog-user/src/blog-user-module/blog-user.module.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { BlogUserRepository } from './blog-user.repository';
 import { BlogUserFactory } from './blog-user.factory';
+import { BlogUserModel, BlogUserSchema } from './blog-user.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: BlogUserModel.name, schema: BlogUserSchema }
+  ])],
   providers: [BlogUserRepository, BlogUserFactory],
   exports: [BlogUserRepository],
 })
