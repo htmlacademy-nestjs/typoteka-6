10d8fc79dd98013d56a795dc9b838b963ed21648
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-03 17:52:22 +0300

    3.10. Извлекаем данные с помощью `ConfigService`.
    Потренируемся в получении данных от `ConfigService`. Для этого внесём небольшие правки в `main.ts`. Все необходимые данные для запуска приложения извлечём из `ConfigService`.

--- project/apps/account/src/main.ts
+++ project/apps/account/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app/app.module';
 
@@ -24,7 +25,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('spec', app, document);
 
-  const port = process.env.PORT || 3000;
+  const configService = app.get(ConfigService);
+  const port = configService.get('application.port');
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
