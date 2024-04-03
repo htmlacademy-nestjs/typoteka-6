ebebaa7f533af08c171e1f9e8dd0a8645e58dc10
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-03 17:57:14 +0300

    3.11. Внедрит `dbConfig` в `AuthenticationService`. WIP
    Этот коммит только для демонстрации возможностей работы с модулем конфигурации. Мы выполнили все подготовительные действия, посмотрим
    как можно получить подготовленные настройки. Есть несколько способов это сделать.
    
    Можно внедрить только нужную часть конфигурации в качестве зависимости. Такой способ позволит воспользоваться всеми преимуществами
    TypeScript: он не даст случайно обратиться к несуществующему ключу конфигурации.
    
    Для демонстрации этого способа потребуется декоратор `@Inject`. Аргументом ему необходимо передать уникальный ключ объекта конфигурации. Его можно получить через свойство `KEY` объекта.
    
    Объект с конфигурацией внедрим в свойство `databaseConfig`. В качестве ограничения на типы укажем `typeof dbConfig`.
    
    Теперь все настройки для подключения к MongoDB доступны через свойство `databaseConfig`.
    
    WIP: Эта функциональность в данный момент времени не требуется. Фрагмент кода представлен для демонстрации возможностей.

--- project/libs/account/config/src/index.ts
+++ project/libs/account/config/src/index.ts
@@ -1,1 +1,4 @@
 export { AccountConfigModule } from './account-config.module';
+
+export { default as applicationConfig } from './configurations/app.config';
+export { default as dbConfig } from './configurations/mongo.config';
