55ac10209129033044e368a2a834c8667b6ac265
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-03 18:28:11 +0300

    3.17. Добавит функцию `getMongoConnectionString`.
    Для установки соединения с MongoDB нам потребуется подготовленная строка соединения (connection string). В этой строке содержится информация о подключении к СУБД: протокол, имя пользователя, пароль, адрес хоста и другие необходимые параметры.
    
    Использовать MongoDB могут несколько сервисов, поэтому разумно
    подготовить отдельную функцию `getMongoConnectionString` и разместить её в общей библиотеке. Для этой цели хорошо подойдёт ранее созданный модуль `common.ts`.
    
    Функция для формирования строки подключения реализуется максимально
    просто: на вход принимает все необходимые значения, а затем, используя интерполяцию, формирует строку для подключения.

--- project/libs/shared/helpers/src/lib/common.ts
+++ project/libs/shared/helpers/src/lib/common.ts
@@ -24,3 +24,7 @@ export function fillDto<T, V extends PlainObject>(
       ...options,
   });
 }
+
+export function getMongoConnectionString({username, password, host, port, databaseName, authDatabase}): string {
+  return `mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=${authDatabase}`;
+}
