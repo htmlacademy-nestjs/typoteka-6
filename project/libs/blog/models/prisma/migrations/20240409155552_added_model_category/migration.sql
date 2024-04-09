67a3d80b495110530bc788c2a7012271a2427b35
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-09 18:56:08 +0300

    4.8. Добавит описание модели `Category` WIP
    Каждая публикация в блоге принадлежит к нескольким категориям (опционально). Поэтому нам потребуется отдельная модель `Category`. Добавим в схему описание
    новой модели, а затем установим связь между `Post` и `Category`.
    
    ```
    npx prisma migrate dev \
    --name "Added model Category" \
    --schema ./shared/blog/models/prisma/schema.prisma \
    --skip-generate
    ```
    
    WIP: Схема в процессе разработки

---
+++ project/libs/blog/models/prisma/migrations/20240409155552_added_model_category/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToPost" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE INDEX "categories_title_idx" ON "categories"("title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToPost_AB_unique" ON "_CategoryToPost"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToPost_B_index" ON "_CategoryToPost"("B");
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPost" ADD CONSTRAINT "_CategoryToPost_A_fkey" FOREIGN KEY ("A") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPost" ADD CONSTRAINT "_CategoryToPost_B_fkey" FOREIGN KEY ("B") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
