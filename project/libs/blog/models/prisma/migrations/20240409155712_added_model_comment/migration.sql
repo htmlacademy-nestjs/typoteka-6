40ce233004d17a044d6e98165089b1c344ea352e
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-09 18:58:45 +0300

    4.9. Добавит описание модели `Comment`. WIP
    Аналогичным образом подготовим модель `Comment`. Она определяет сущность «Комментарий» в блоге. Эта модель непосредственно связана с моделью `Post`. Мы можем получить все комментарии к публикации и получить публикации к которой оставлен комментарий.
    
    Для установки связи используем атрибут `@relation`. Он применяется только к полям отношения. К скалярным полям его применить нельзя.
    
    ```
    npx prisma migrate dev \
    --name "Added model Comment" \
    --schema ./prisma/schema.prisma \
    --skip-generate
    ```
    
    WIP: Схема в процессе разработки

---
+++ project/libs/blog/models/prisma/migrations/20240409155712_added_model_comment/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "comments" (
+    "id" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "post_id" TEXT NOT NULL,
+
+    CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "comments_post_id_idx" ON "comments"("post_id");
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
