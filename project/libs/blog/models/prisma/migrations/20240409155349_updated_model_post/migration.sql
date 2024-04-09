363edad7df925b69f66f2c0bdfd3a722758e9e80
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-09 18:54:31 +0300

    4.7. Изменит название полей и таблицы. WIP
    В предыдущем шаге мы описали первую модель `Post` и синхронизировали схему Prisma со схемой БД. Обратите внимание, что получилось на выходе.
    
    В базу была добавлена таблица `Post` со всеми полями. Всё здорово, но обычно мы ожидаем видеть имена в `snake_case` для полей (на уровне) БД, а названия таблиц во множественном числе.
    
    Чтобы исправить это, мы можем добавить модификатор `@@Map()` в описание схемы и передать произвольное имя для таблицы. Если требуется дать другое имя
    для поля на уровне БД воспользуйтесь модификатором `@map`.
    
    После внесения изменений в схему, сгенерируем миграцию:
    
    ```
    npx prisma migrate dev \
    --name "Updated model Post" \
    --schema ./prisma/schema.prisma \
    --skip-generate
    ```
    
    WIP: Схема в процессе разработки

---
+++ project/libs/blog/models/prisma/migrations/20240409155349_updated_model_post/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Post";
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "posts_title_idx" ON "posts"("title");
