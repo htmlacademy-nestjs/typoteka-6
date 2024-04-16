f453ffbf2937e0b2bfd380b2647b26ba2980b14d
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-16 19:41:51 +0300

    5.1. Добавит базовую валидацию для сервиса `account`.
    Вопрос валидации в Nest решается несколькими способами. С точки зрения самих проверок мы вправе использовать любую библиотеку: Joi,
    class-validator или любую другую.
    
    С точки зрения реализации кода проверок тоже есть несколько путей.
    
    Применение пайпов — самый оптимальный из них. Пайпы — это встроенный механизм (абстракция), которую используют для решения двух задач:
    
    * Трансформация данных;
    * Валидация данных.
    
    Пайпы срабатывают до обработчика маршрута. За счёт этого и становятся
    возможными перечисленные сценарии. Добавим валидацию для сервиса `account`.
    
    Сам процесс валидации сделаем с помощью пакета `class-validator`.
    
    Расставим все необходимые декораторы в DTO и опишем константы с сообщениями об ошибках.

--- project/libs/account/authentication/src/dto/create-user.dto.ts
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -1,33 +1,41 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsISO8601, IsString } from 'class-validator';
+
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'User unique address',
     example: 'user@user.ru'
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'User birth date',
     example: '1981-03-12',
   })
+  @IsISO8601({}, { message: AuthenticationValidateMessage.DateBirthNotValid })
   public dateBirth: string;
 
   @ApiProperty({
     description: 'User first name',
     example: 'Keks',
   })
+  @IsString()
   public firstname: string;
 
   @ApiProperty({
     description: 'User last name',
     example: 'Ivanov'
   })
+  @IsString()
   public lastname: string;
 
   @ApiProperty({
     description: 'User last name',
     example: 'Ivanov'
   })
+  @IsString()
   public password: string;
 }
--- project/libs/account/authentication/src/dto/login-user.dto.ts
+++ project/libs/account/authentication/src/dto/login-user.dto.ts
@@ -1,15 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
+
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
 
 export class LoginUserDto {
   @ApiProperty({
     description: 'User uniq email',
     example: 'user@user.ru',
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
 
   @ApiProperty({
     description: 'User password',
     example: '123456'
   })
+  @IsString()
   public password: string;
 }
