20cf6c287d98a22e450d39dbeb24cd46a673c82f
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-16 19:41:51 +0300

    5.1. Добавит базовую валидацию для сервиса `account`.
    Вопрос валидации в Nest решается несколькими способами. С точки зрения самих проверок мы вправе использовать любую библиотеку: Joi,
    class-validator или любую другую.
    
    С точки зрения реализации кода проверок тоже есть несколько путей.
    
    Применение пайпов — самый оптимальный из них. Пайпы — это встроенный механизм (абстракция), которую используют для решения двух задач:
    
    * Трансформация данных;
    * Валидация данных.
    
    Пайпы срабатывают до обработчика маршрута. За счёт этого и становятся
    возможными перечисленные сценарии. Добавим валидацию для сервиса `account`.
    
    Сам процесс валидации сделаем с помощью пакета `class-validator`.
    
    Расставим все необходимые декораторы в DTO и опишем константы с сообщениями об ошибках.

--- project/libs/account/authentication/src/authentication-module/authentication.constant.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.constant.ts
@@ -10,3 +10,8 @@ export const AuthenticationResponseMessage = {
   UserExist: 'User with the email already exists',
   UserCreated: 'The new user has been successfully created.',
 } as const;
+
+export const AuthenticationValidateMessage = {
+  EmailNotValid: 'The email is not valid',
+  DateBirthNotValid: 'The user date birth is not valid',
+} as const;
