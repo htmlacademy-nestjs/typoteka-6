cf2ebef3820c9f07cf4754543031c087e8538c03
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-17 17:46:38 +0300

    5.10. Добавит `BlogPostEntity`. WIP
    Добавим описание сущности `BlogPostEntity`. Этот код похож на другие сущности, которые мы определяли ранее. Наиболее интересным местом является метод `populate` и `toPOJO`. Сущность `BlogPostEntity` сложней чем остальные, так как включает в себя связь с другими сущностями: `BlogCategoryEntity` и `BlogCommentEntity`.
    
    Из-за этого приходится сделать чуть больше телодвижений для создания экземпляра новой `BlogPostEntity`. При создании экземпляра сущности публикации также создать экземпляры `BlogCategoryEntity` и `BlogCommentEntity`.
    
    Для создания экземпляров `BlogCategoryEntity` и `BlogCommentEntity` мы используем заранее подготовленные фабрики.

--- project/libs/blog/blog-category/src/index.ts
+++ project/libs/blog/blog-category/src/index.ts
@@ -1,1 +1,6 @@
 export { BlogCategoryModule } from './blog-category-module/blog-category.module';
+export { BlogCategoryEntity } from './blog-category-module/blog-category.entity';
+export { BlogCategoryFactory } from './blog-category-module/blog-category.factory';
+export { BlogCategoryService } from './blog-category-module/blog-category.service';
+
+export { CategoryRdo } from './blog-category-module/rdo/category.rdo';
--- project/libs/blog/blog-comment/src/index.ts
+++ project/libs/blog/blog-comment/src/index.ts
@@ -1,1 +1,3 @@
 export { BlogCommentModule } from './blog-comment-module/blog-comment.module';
+export { BlogCommentEntity } from './blog-comment-module/blog-comment.entity';
+export { BlogCommentFactory } from './blog-comment-module/blog-comment.factory';
