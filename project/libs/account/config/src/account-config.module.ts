35685643f7857ac55b83a417adbfb6793bb567fc
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-17 18:55:23 +0300

    5.19 Добавит настройки для JWT.
    Для формирования JWT потребуются минимум два значения:
    
    * Секрет. Он будет использоваться для формирования подписи токена;
    * Время жизни токена. Оно всегда ограничено и нужно определиться сколько времени будет валиден токен.
    
    Эти значения могут меняться, поэтому их нужно передавать снаружи. Создадим для сервиса `accounts` конфигурацию для JWT. Определим две настройки, которые будем задавать через переменные окружения:
    
    `JWT_ACCESS_TOKEN_SECRET` и `JWT_ACCESS_TOKEN_EXPIRES_IN`.

--- project/libs/account/config/src/account-config.module.ts
+++ project/libs/account/config/src/account-config.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import applicationConfig from './configurations/app.config';
 import mongoConfig from './configurations/mongo.config';
+import jwtConfig from './configurations/jwt.config';
 
 const ENV_USERS_FILE_PATH = 'apps/account/account.env';
 
@@ -11,7 +12,7 @@ const ENV_USERS_FILE_PATH = 'apps/account/account.env';
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      load: [applicationConfig, mongoConfig],
+      load: [applicationConfig, mongoConfig, jwtConfig],
       envFilePath: ENV_USERS_FILE_PATH
     }),
   ]
--- project/libs/account/config/src/index.ts
+++ project/libs/account/config/src/index.ts
@@ -2,5 +2,6 @@ export { AccountConfigModule } from './account-config.module';
 
 export { default as applicationConfig } from './configurations/app.config';
 export { default as dbConfig } from './configurations/mongo.config';
+export { default as jwtConfig } from './configurations/jwt.config';
 
 export { getMongooseOptions } from './configurations/mongodb/get-mongoose-options';
