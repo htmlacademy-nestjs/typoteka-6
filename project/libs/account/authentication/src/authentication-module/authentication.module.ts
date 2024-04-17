a5a84105d0bd24bcea9549c62c92d595f04ad64c
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-17 19:02:48 +0300

    5.21. Подключит `JwtModule` в `AuthenticationModule`. WIP
    Чтобы сформировать токен нам потребуется функциональность `JwtModule`, который мы установили с пакетом `@nestjs/jwt`. Код для создания токена разместим в сервисе `AuthenticationService`. Поэтому сам модуль (`JwtModule`) подключим в `AuthenticationModule`.
    
    Для конфигурирования модуля воспользуемся методом `registerAsync`. Принцип работы  подобных методов мы уже разбирали. Всю необходимую конфигурацию вернёт функция `getJWTOption`, которую мы подготовили ранее. Поскольку ей потребуется доступ к `ConfigService`, не забудем внедрить его в качестве зависимости. Укажем `inject`.
    
    Также нам потребуется дополнительный провайдер. В нём мы реализуем стратегию `JwtAccessStrategy`. Стратегии PassportJS — это модуль, который обеспечивает методы аутентификации для конкретной стратегии.
    
    Код класса `JwtAccessStrategy` разберём позже. Пока просто укажем его подключение. Реализацию выполним позже.
    
    WIP: `JwtAccessStrategy` ещё не реализован.

--- project/libs/account/authentication/src/authentication-module/authentication.module.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 import { BlogUserModule } from '@project/blog-user';
+import { getJwtOptions } from '@project/account-config';
 
+import { JwtAccessStrategy } from '../strategies/jwt-access.strategy';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
-  imports: [BlogUserModule],
+  imports: [
+    BlogUserModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: getJwtOptions,
+    })
+  ],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService]
+  providers: [
+    AuthenticationService,
+    JwtAccessStrategy,
+  ]
 })
 export class AuthenticationModule {}
