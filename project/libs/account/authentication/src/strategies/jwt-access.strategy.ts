86d0029f351be4b58224bc479fe8776853972789
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-17 19:09:02 +0300

    5.23. Добавит стратегию `JwtAccessStrategy`. WIP
    Стратегия в Passport JS — это модуль, который обеспечивает методы аутентификации для конкретной стратегии аутентификации, такой как аутентификация через социальную сеть или локальная аутентификация.
    
    Стратегии Passport JS реализуются в виде отдельных модулей и предоставляют определенный интерфейс для аутентификации пользователей. Они позволяют задать логику аутентификации,
    используя специфичные для каждой стратегии параметры и методы.
    
    Стратегии Passport JS состоят из двух основных частей: функции-конструктора стратегии и метода аутентификации. Функция-конструктор определяет параметры стратегии, такие как  имя, параметры конфигурации и другие настройки. Метод аутентификации предоставляет способ проверки и аутентификации пользователя, используя различные методы и стратегии.
    
    Информацию о JWT-токене будем передавать в заголовке авторизации. Заголовок `Authorization` с префиксом Bearer используется для передачи токена аутентификации в HTTP-запросах. Формат заголовка выглядит следующим образом: `Authorization: Bearer <token>`, где <token> - это сам токен аутентификации.
    
    Этот заголовок используется для аутентификации пользователя при доступе к защищенным ресурсам в веб-приложениях. Когда пользователь успешно проходит аутентификацию, сервер выдает токен аутентификации, который затем передается в заголовке `Authorization` с префиксом Bearer при каждом последующем запросе на защищенный ресурс.

---
+++ project/libs/account/authentication/src/strategies/jwt-access.strategy.ts
@@ -0,0 +1,23 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { TokenPayload } from '@project/shared/core';
+
+@Injectable()
+export class JwtAccessStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('jwt.accessTokenSecret')
+    });
+  }
+
+  public async validate(payload: TokenPayload) {
+    return payload;
+  }
+}
