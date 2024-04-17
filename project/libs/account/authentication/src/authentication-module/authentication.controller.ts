e95d59aae82c70e7bc059d998091f300fc759ead
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-17 19:14:03 +0300

    5.25. Добавит поддержку JWT в `AuthenticationController`.
    Соберём всё вместе и добавим поддержку формирования токенов. Для этого обновить обработчик `login`. После верификации пользователя, подготовим токен. Затем вернём его клиенту. Для этого соединим результат `verifiedUser.toPOJO` с `userToken`. В RDO соответствующие поле (`token`) присутствуют.
    
    Можно запустить приложение и попробовать пройти аутентификацию. Приложение вернёт сформированный JWT Token.
    
    Токен формируется, но пока от него не сильно много пользы. Все маршруты на текущий момент публичны. Исправим это и нам помогут Guard.
    
    Защитим маршрут `/:id` (метод `show`) с помощью `JwtAuthGuard`, который мы создали в предыдущем шаге. Чтобы применить Guard, необходимо воспользоваться декоратором `UseGuards`. Декоратор применяется к методу-обработчику.
    
    Теперь обработчик `show` защищён. Чтобы получить информацию по пользователю, нужно добавить к запросу заголовок `Authorization` со значением `Bearer <token>`, где место token должен быть выданный вам JWT-токен. Пример запроса добавлен в `authentication.http`.

--- project/libs/account/authentication/src/authentication-module/authentication.controller.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, UseGuards } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+
+import { MongoIdValidationPipe } from '@project/pipes';
 
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from '../dto/create-user.dto';
@@ -7,7 +9,8 @@ import { LoginUserDto } from '../dto/login-user.dto';
 import { LoggedUserRdo } from '../rdo/logged-user.rdo';
 import { UserRdo } from '../rdo/user.rdo';
 import { AuthenticationResponseMessage } from './authentication.constant';
-import { MongoIdValidationPipe } from '@project/pipes';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { fillDto } from '@project/shared/helpers';
 
 @ApiTags('authentication')
 @Controller('auth')
@@ -42,7 +45,8 @@ export class AuthenticationController {
   @Post('login')
   public async login(@Body() dto: LoginUserDto) {
     const verifiedUser = await this.authService.verifyUser(dto);
-    return verifiedUser.toPOJO();
+    const userToken = await this.authService.createUserToken(verifiedUser);
+    return fillDto(LoggedUserRdo, { ...verifiedUser.toPOJO(), ...userToken });
   }
 
   @ApiResponse({
@@ -60,6 +64,7 @@ export class AuthenticationController {
     return existUser.toPOJO();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('/demo/:id')
   public async demoPipe(@Param('id') id: number) {
     console.log(typeof id);
