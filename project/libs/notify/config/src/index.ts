5bdcc53cf9ee852f8613f81e2c8729ed04b31e11
Igor Antonov <antonov.igor.khv@gmail.com>
2024-04-24 19:01:40 +0300

    6.20. Настройки и заготовка для `email-subscriber`
    По аналогии с другими сервисами подготовим отдельную библиотеку для
    конфигурирования `notify`. Нам потребуются настройки для подключения к MongoDB и Rabbit.

---
+++ project/libs/notify/config/src/index.ts
@@ -0,0 +1,3 @@
+export { NotifyConfigModule } from './notify-config.module';
+export { default as NotifyConfig } from './notify.config';
+export { getMongooseOptions } from './notify.mongoose-options';
