f85944e4f1f70a699e8783886f95ff110f635c16
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-01 22:24:03 +0300

    7.14. Обновит стратегию `JwtRefreshStrategy`
    Помимо валидации токена и проверки его времени жизни, добавим проверку на наличие токена в базе данных. Если токена нет, то бросим исключение `TokenNotExistsException`.
    
    Если проверка пройдена, удалим старый Refresh Token, а также удалим все возможные просроченные токены.

--- project/libs/account/authentication/src/strategies/jwt-refresh.strategy.ts
+++ project/libs/account/authentication/src/strategies/jwt-refresh.strategy.ts
@@ -4,15 +4,18 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigType } from '@nestjs/config';
 
 import { jwtConfig } from '@project/account-config';
-import { TokenPayload } from '@project/shared/core';
+import { RefreshTokenPayload } from '@project/shared/core';
 
 import { AuthenticationService } from '../authentication-module/authentication.service';
+import { RefreshTokenService } from '../refresh-token-module/refresh-token.service';
+import { TokenNotExistsException } from '../exceptions/token-not-exists.exception';
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor(
     @Inject(jwtConfig.KEY) private readonly jwtOptions: ConfigType<typeof jwtConfig>,
-    private readonly authService: AuthenticationService
+    private readonly authService: AuthenticationService,
+    private readonly refreshTokenService: RefreshTokenService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -20,7 +23,14 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh'
     });
   }
 
-  public async validate(payload: TokenPayload) {
+  public async validate(payload: RefreshTokenPayload) {
+    if (! await this.refreshTokenService.isExists(payload.tokenId)) {
+      throw new TokenNotExistsException(payload.tokenId);
+    }
+
+    await this.refreshTokenService.deleteRefreshSession(payload.tokenId);
+    await this.refreshTokenService.deleteExpiredRefreshTokens();
+
     return this.authService.getUserByEmail(payload.email);
   }
 }
