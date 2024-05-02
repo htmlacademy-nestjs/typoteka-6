dda8d63bc579fee3cbfde9f4cae455a29354e55d
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-02 19:18:01 +0300

    7.25. Подключит `RequestIdInterceptor`.
    Подключим глобально `RequestIdInterceptor`. Каждому запросу будем добавлять идентификатор.

--- project/apps/api/src/main.ts
+++ project/apps/api/src/main.ts
@@ -6,12 +6,16 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { RequestIdInterceptor } from '@project/interceptors';
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalInterceptors(new RequestIdInterceptor());
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
