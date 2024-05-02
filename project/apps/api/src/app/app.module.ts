6980aab7a3b3c82b4dfe5d121e87946592541e43
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-02 18:03:38 +0300

    7.21. Подключит `HttpModule` в `app.module.ts`.
    Подключим и сконфигурируем Axios. Для этого в `app.module.ts` подключим `HttpModule`. Укажем две настройки: `timeout` и `maxRedirects`.

--- project/apps/api/src/app/app.module.ts
+++ project/apps/api/src/app/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+
+import {
+  HTTP_CLIENT_MAX_REDIRECTS,
+  HTTP_CLIENT_TIMEOUT
+} from './app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule.register({
+      timeout: HTTP_CLIENT_TIMEOUT,
+      maxRedirects: HTTP_CLIENT_MAX_REDIRECTS,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
