63c097d094e03d37758ff39e2eab6a3cb458c8b9
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-02 19:11:02 +0300

    7.22. FIX. `BaseMongoRepository`.
    Исправляет настройки для конвертации в PlainObject

--- project/libs/shared/data-access/src/repository/base-mongo.repository.ts
+++ project/libs/shared/data-access/src/repository/base-mongo.repository.ts
@@ -20,7 +20,7 @@ export abstract class BaseMongoRepository<
       return null;
     }
 
-    const plainObject = document.toObject({ versionKey: false }) as ReturnType<T['toPOJO']>;
+    const plainObject = document.toObject({ getters: true, versionKey: false, flattenObjectIds: true })  as ReturnType<T['toPOJO']>;
     return this.entityFactory.create(plainObject);
   }
 
