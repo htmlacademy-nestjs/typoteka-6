0bd98c474671124b5f3ffc01fa8e7af79ffedb45
Igor Antonov <antonov.igor.khv@gmail.com>
2024-05-02 17:56:32 +0300

    7.19. Добавит конфигурацию для `api`.
    Сервис API должен знать, где запущены остальные микросервисы. В самых простых случаях, информация об этом может размещаться в конфигурации сервиса `api`. Но этот вариант подойдёт совсем для простых случаев.
    
    Как правило, при использовании микросервисной архитектуры, за оркестрацию и обнаружение отвечают отдельные сервисы (инструменты).
    
    Для демонстрации API Gateway мы рассмотрим простой вариант: опишем свой сервис и подготовим для него конфигурацию. Её вы можете передавать через переменные окружения по аналогии с другими сервисами. Мы упростим задачу и выполним конфигурирование прямо в модуле.

---
+++ project/apps/api/src/app/app.config.ts
@@ -0,0 +1,7 @@
+export enum ApplicationServiceURL {
+  Users = 'http://localhost:3333/api/auth',
+  Blog = 'http://localhost:3334/api/posts'
+}
+
+export const HTTP_CLIENT_MAX_REDIRECTS = 5;
+export const HTTP_CLIENT_TIMEOUT = 3000;
